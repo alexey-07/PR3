3e73ecc2fe7fe332a0fd63d584f56ec49d6ecc9c
Алексей Леонтьев <leontev.leha06@bk.ru>
2024-12-05 12:02:51 +0500

    Вывел данные из таблицы Partners

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            label_Partners = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 114);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(384, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Данные:";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label_Partners);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 113);
+            panel2.Name = "panel2";
+            panel2.Padding = new Padding(20);
+            panel2.Size = new Size(800, 337);
+            panel2.TabIndex = 1;
+            // 
+            // label_Partners
+            // 
+            label_Partners.Dock = DockStyle.Fill;
+            label_Partners.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label_Partners.Location = new Point(20, 20);
+            label_Partners.Name = "label_Partners";
+            label_Partners.Size = new Size(760, 297);
+            label_Partners.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label_Partners;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace PR3
 {
     public partial class Form1 : Form
@@ -5,6 +7,25 @@ namespace PR3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (PartnersContext db = new PartnersContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var partners = db.Partners.ToList();
+                foreach (Partner u in partners)
+                {
+                    label_Partners.Text+=($"{u.Id} {u.IdTypeOfPartner} {u.NameProduct} {u.Address} {u.Inn} {u.DirectorsName} {u.MobilePhone} {u.Email}  {u.Rating}\n");
+                }
+            }
+
         }
     }
 }
