10cebcb9634196668a5e14ccb0c90cfe3865a364
User <User@WIN-03GCEUPGV2O>
2024-12-05 10:10:20 +0500

    Add project files.

---
+++ TypeProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PR3;
+
+public partial class TypeProduct
+{
+    public short Id { get; set; }
+
+    public string TypeProduct1 { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
